459504349e28182f831393934c33208bd83a7328
Micael Ferreira <micael@localhost.localdomain>
2020-01-17 19:53:56 -0300

    Creating a new user

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\User;
+use App\api\ApiError;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = ['data' => $this->user->all()];
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try{
+            $userData = $request->all();
+            $this->user->create($userData);
+
+            $return = ['data' => ['msg' => 'Usuário criado com sucesso!']];
+
+            return response()->json($return, 201);
+        } catch (\Exception $e) {
+            if (config('app.debug')) {
+                return response()->json(ApiError::errorMessage($e->getMessage(), 01), 500);
+            }
+            return response()->json(ApiError::errorMessage('Erro interno', 01), 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = $this->user->find($id);
+        
+        if(!$user){
+            return response()->json(ApiError::errorMessage('Usuário não encontrado', 04), 404);
+        }
+
+        $data = ['data' => $user];
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        try{
+            $userData = $request->all();
+            $user = $this->user->find($id);
+            $user->update($userData);
+            $return = ['data' => ['msg' => 'Usuário atualizado com sucesso!']];
+
+            return response()->json($return, 201);
+        } catch (\Exception $e) {
+            if (config('app.debug')) {
+                return response()->json(ApiError::errorMessage($e->getMessage(), 02), 500);
+            }
+            return response()->json(ApiError::errorMessage('Erro interno', 02), 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $id)
+    {
+        try{
+            $id->delete();
+
+            return response()->json(['data' => ['msg' => 'Usuário ' . $id->name . ' removido com sucesso!']], 200);
+        } catch (\Exception $e) {
+            if (config('app.debug')) {
+                return response()->json(ApiError::errorMessage($e->getMessage(), 03), 500);
+            }
+            return response()->json(ApiError::errorMessage('Erro interno', 03), 500);
+        }
+    }
+}
