81b1399f00182e6dda5d580b32b5c26019ebe1e8
Micael Ferreira <micael@localhost.localdomain>
2020-01-16 23:22:38 -0300

    resolve conflict

--- database/migrations/2020_01_15_010112_create_table_sponsors.php
+++ database/migrations/2020_01_15_010112_create_table_sponsors.php
@@ -18,7 +18,6 @@ class CreateTableSponsors extends Migration
             $table->string('name');
             $table->string('type');
             $table->string('image');
-<<<<<<< HEAD
             $table->bigInteger('user_id')->unsigned();
             $table->timestamps();
             
@@ -26,16 +25,6 @@ class CreateTableSponsors extends Migration
             $table->foreign('user_id')
                 ->references('id')->on('users')
                 ->onDelete('cascade');
-=======
-            
-            //event
-            $table->bigInteger('event_id')->unsigned();
-            $table->foreign('user_id')
-                ->references('id')->on('users')
-                ->onDelete('cascade');
-            
-            $table->timestamps();
->>>>>>> dcb4ac0fd92195495bef9dc428acd5ab46fcd18b
         });
     }
 
