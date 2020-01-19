2b8a3861c648d923a55bcb907f00a11273b6e55f
Micael Ferreira <micaelmf2@gmail.com>
2020-01-18 22:00:32 -0300

    updating routes

--- routes/api.php
+++ routes/api.php
@@ -13,16 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
-
-/*
-Route::namespace('Api')->name('api.')->group(function(){
-    Route::prefix('event')->group(function(){
-        Route::get('/', 'EventController@index')->name('events');
-        Route::get('/{id}', 'EventController@show')->name('event');
-    });
 });
 */
 
@@ -33,6 +26,16 @@ Route::namespace('Api')->name('api.')->group(function(){
         Route::post('/', 'EventController@store')->name('store_event');
         Route::put('/{id}', 'EventController@update')->name('update_event');
         Route::delete('/{id}', 'EventController@destroy')->name('destroy_event');
+    });
+});
+
+Route::namespace('Api')->name('api.')->group(function(){
+    Route::prefix('addresses')->group(function(){
+        Route::get('/', 'AddressController@index')->name('index_Addresses');
+        Route::get('/{id}', 'AddressController@show')->name('single_Address');
+        Route::post('/', 'AddressController@store')->name('store_Address');
+        Route::put('/{id}', 'AddressController@update')->name('update_Address');
+        Route::delete('/{id}', 'AddressController@destroy')->name('destroy_Address');
     });
 });
 
@@ -53,5 +56,35 @@ Route::namespace('Api')->name('api.')->group(function(){
         Route::post('/', 'ActivityController@store')->name('store_Activity');
         Route::put('/{id}', 'ActivityController@update')->name('update_Activity');
         Route::delete('/{id}', 'ActivityController@destroy')->name('destroy_Activity');
+    });
+});
+
+Route::namespace('Api')->name('api.')->group(function(){
+    Route::prefix('speakers')->group(function(){
+        Route::get('/', 'SpeakerController@index')->name('index_Speakers');
+        Route::get('/{id}', 'SpeakerController@show')->name('single_Speaker');
+        Route::post('/', 'SpeakerController@store')->name('store_Speaker');
+        Route::put('/{id}', 'SpeakerController@update')->name('update_Speaker');
+        Route::delete('/{id}', 'SpeakerController@destroy')->name('destroy_Speaker');
+    });
+});
+
+Route::namespace('Api')->name('api.')->group(function(){
+    Route::prefix('participants')->group(function(){
+        Route::get('/', 'ParticipantController@index')->name('index_Participants');
+        Route::get('/{id}', 'ParticipantController@show')->name('single_Participant');
+        Route::post('/', 'ParticipantController@store')->name('store_Participant');
+        Route::put('/{id}', 'ParticipantController@update')->name('update_Participant');
+        Route::delete('/{id}', 'ParticipantController@destroy')->name('destroy_Participant');
+    });
+});
+
+Route::namespace('Api')->name('api.')->group(function(){
+    Route::prefix('sponsors')->group(function(){
+        Route::get('/', 'SponsorController@index')->name('index_Sponsors');
+        Route::get('/{id}', 'SponsorController@show')->name('single_Sponsor');
+        Route::post('/', 'SponsorController@store')->name('store_Sponsor');
+        Route::put('/{id}', 'SponsorController@update')->name('update_Sponsor');
+        Route::delete('/{id}', 'SponsorController@destroy')->name('destroy_Sponsor');
     });
 });
