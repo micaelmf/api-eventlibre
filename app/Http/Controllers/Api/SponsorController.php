65189d3b3fa010748c7400ff79ef8b24036cf683
Micael Ferreira <micaelmf2@gmail.com>
2020-01-18 22:00:07 -0300

    creating controller of sponsor

---
+++ app/Http/Controllers/Api/SponsorController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\api\ApiError;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Sponsor;
+
+class SponsorController extends Controller
+{
+    public function __construct(Sponsor $sponsor)
+    {
+        $this->sponsor = $sponsor;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = ['data' => $this->sponsor->all()];
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try{
+            $sponsorData = $request->all();
+            $this->sponsor->create($sponsorData);
+
+            $return = ['data' => ['msg' => 'Empresa criada com sucesso!']];
+
+            return response()->json($return, 201);
+        } catch (\Exception $e) {
+            if (config('app.debug')) {
+                return response()->json(ApiError::errorMessage($e->getMessage(), 01), 500);
+            }
+            return response()->json(ApiError::errorMessage('Erro Interno', 01), 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $sponsor = $this->sponsor::with(['event'])->find($id);
+        
+        if(!$sponsor){
+            return response()->json(ApiError::errorMessage('Empresa não encontrada', 04), 404);
+        }
+
+        $sponsor = collect($sponsor)->except(['event_id']);
+        $data = ['data' => $sponsor];
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        try{
+            $sponsorData = $request->all();
+            $sponsor = $this->sponsor->find($id);
+            $sponsor->update($sponsorData);
+            $return = ['data' => ['msg' => 'Empresa atualizada com sucesso!']];
+
+            return response()->json($return, 201);
+        } catch (\Exception $e) {
+            if (config('app.debug')) {
+                return response()->json(ApiError::errorMessage($e->getMessage(), 02), 500);
+            }
+            return response()->json(ApiError::errorMessage('Erro Interno', 02), 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try{
+            $id->delete();
+
+            return response()->json(['data' => ['msg' => 'Empresa ' . $id->name . ' removida com sucesso!']], 200);
+        } catch (\Exception $e) {
+            if (config('app.debug')) {
+                return response()->json(ApiError::errorMessage($e->getMessage(), 03), 500);
+            }
+            return response()->json(ApiError::errorMessage('Erro Interno', 03), 500);
+        }
+    }
+}
--- app/Sponsor.php
+++ app/Sponsor.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sponsor extends Model
 {
+    protected $fillable = [
+        'name',
+        'type',
+        'image',
+        'event_id'
+    ];
+
     /**
      * Get the event for the sponsor.
      */
